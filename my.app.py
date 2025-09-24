602b7d8e6f775b79dbcad872707e9bf7a6dbcb5e
fenggeHan <1198310863@qq.com>
2025-09-24 14:25:05 +0800

    Update my.app.py

--- my.app.py
+++ my.app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Gold mineralization prediction", layout="wide")
 st.title("Gold Mineralization Prediction")
 st.write("Prediction of Gold Mineralization Potential in Magmatic Rocks​")
 st.write("Developer: Dr. Fengge Han; School of Science, East China University of Science and Technology, Nanchang, China")
-st.write("Developer: Dr. Chengbiao Leng; School of Earth and Planetary Sciences, East China University of Science and Technology, Nanchang, China")
+st.write("Developer: Prof. Chengbiao Leng; School of Earth and Planetary Sciences, East China University of Science and Technology, Nanchang, China")
 st.write("Email: hanfengge@ecut.edu.cn(Han F.G.)")
 
 # ===== 特征列和标签列 =====
@@ -133,6 +133,7 @@ if new_file is not None:
             file_name="prediction_results.csv",
             mime="text/csv"
         )
+
 
 
 
